0af6e7952d7560c26ae348f16f60e7c47f04f96b
jotazz25 <jalvarez@tecbaco.com>
2024-01-30 16:31:29 -0500

    nueva practica con tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,8 @@
-import { Fragment, useEffect, useState } from "react";
-import Formulario from "./components/Formulario";
-import Films from "./components/Films";
-
-const registers = JSON.parse(localStorage.getItem('peliculas')) || [
-    {
-        id:1,
-        title: "Moby Dick",
-        description: "Ballena",
-        state: "Disponible",
-        testing: true
-    },
-    {
-        id:2,
-        title: "Benjamin Buttom",
-        description: "Niño Viejo",
-        state: "No Disponible",
-        testing: true
-    },
-    {
-        id:3,
-        title: "Avengers",
-        description: "Iniciativa Vengadores",
-        state: "Disponible",
-        testing: false
-    }
-]
-
 const App = () => {
-
-    const [films, setFilm] = useState(registers)
-
-    useEffect(() => {
-        localStorage.setItem('peliculas',JSON.stringify(films))
-    },[films])
-
-    return (
-        <div className="container">
-            <Formulario films={films} setFilm={setFilm}/>
-            <Films films={films} setFilm={setFilm}/>
-        </div>
+    return(
+        <>
+            <h1 className="text-center md:text-right bg-red-500 text-white">HOLA MUNDO</h1>
+        </>
     )
 }
 
